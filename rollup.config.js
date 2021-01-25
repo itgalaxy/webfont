8373d40df0c9e47112aa73edde2f3cbe22a8a878
Jimmy Andrade <jimmy.andrade@facio.com>
2021-01-25 16:51:28 -0300

    build: add rollup build config

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+const { dependencies } = require("./package.json");
+
+const external = [
+  ...Object.keys(dependencies),
+  "crypto",
+  "fs",
+  "path",
+  "stream",
+  "svgicons2svgfont/src/filesorter",
+  "svgicons2svgfont/src/metadata",
+]
+
+const dir = "dist/rollup"
+
+module.exports = [
+  {
+    external,
+    input: ["src/cli.js"],
+    output: {
+      banner: "#!/usr/bin/env node\n",
+      dir,
+      exports: "auto",
+      format: "cjs",
+      preserveModules: false
+    }
+  }, {
+    external,
+    input: "src/index.js",
+    output: {
+      dir,
+      exports: "named",
+      format: "cjs",
+      preserveModules: true
+    }
+  }
+];

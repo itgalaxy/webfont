c6fa83ea130b41312d26b107b8f6fa7c42d6e1ae
Jimmy Andrade <jimmy.andrade@facio.com>
2021-06-05 15:19:42 -0300

    refactor(types): allow importing types from type root directory

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type { Format } from "../types/Format";
+export type { GlyphMetadata } from "./GlyphMetadata";
+export type { GlyphTransformFn } from "./GlyphTransformFn";
+export type { GlyphData } from "./GlyphData";
+export type { InitialOptions } from "./InitialOptions";
+export type { WebfontOptions } from "./WebfontOptions";

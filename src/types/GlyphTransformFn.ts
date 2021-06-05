a3bde0ccc86e4ca25f5e76cbae9e8e5f202abb56
Jimmy Andrade <jimmy.andrade@facio.com>
2021-06-05 15:19:02 -0300

    feat(types): allow `unicode` key and Promise as `GlyphTransformFn` output

--- src/types/GlyphTransformFn.ts
+++ src/types/GlyphTransformFn.ts
@@ -1,4 +1,4 @@
 import {GlyphMetadata} from "./GlyphMetadata";
 
 // eslint-disable-next-line no-unused-vars
-export type GlyphTransformFn = (obj: GlyphMetadata) => Record<"name", string>;
+export type GlyphTransformFn = (obj: GlyphMetadata) => GlyphMetadata | Promise<GlyphMetadata>;
